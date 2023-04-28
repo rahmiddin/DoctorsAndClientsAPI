9d47ebc8cdb7c1d715b84d5b8dde7b242bc0913b
Rahmidin <rahmiddin.hasanov@bk.ru>
2023-04-28 10:52:08 +0500

    Add_requirements_and_env

--- api/backend/backend/settings.py
+++ api/backend/backend/settings.py
@@ -11,18 +11,31 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# ENV
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+env = environ.Env(
+    DEBUG=bool,
+    SECRET_KEY=str,
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g+z&-)dp(s_dc!stb8t(y-ksdrequ-ezebn2gh%u@y*(r!kfx3'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
+
 
 ALLOWED_HOSTS = []
 
